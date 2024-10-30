4286b2e8f83e0c8fa2e8397a65ac028b16d8d41e
Mazen Mohamed Nayef <mazennayef2002@gmail.com>
2024-10-30 23:49:13 +0300

    1- made changing directory "cd" function that changes the path of the directory. 2- Handled the back operation of the cd so we can return back from the directory we are in right now 3- Made 3 Testing cases for the cd operation.

--- src/Repositories/CommandsRepository.java
+++ src/Repositories/CommandsRepository.java
@@ -6,7 +6,31 @@ import java.util.ArrayList;
 
 public class CommandsRepository {
     public String pwd(File directory) throws IOException {
-        System.out.println("BlAH");
         return directory.getCanonicalPath();
+    }
+
+    public String cd (File _workingDirectory, ArrayList<String> inputs) throws Exception {
+        String directory_name = inputs.getFirst();
+        String current_directory = _workingDirectory.toString();
+        // Here to make the ./ return back
+        if (directory_name.equals("./")) {
+            for (int i = current_directory.length() - 1; i >= 0; i-- ) {
+                if (current_directory.charAt(i) == '\\') {
+                    // We make the current_directory the same without the last folder
+                    directory_name = current_directory.substring(0, i);
+                    break;
+                }
+            }
+        }
+        else {
+            // We add the directory to the _workingdirectory
+            directory_name = _workingDirectory.toString() + "/" + directory_name;
+        }
+        // We made a file to see if it exists and isDirectory or not
+        File directory = new File(directory_name);
+        if (directory.exists() && directory.isDirectory()) {
+            return directory_name;
+        }
+        throw new Exception("Not Valid Path");
     }
 }
--- src/Tests/CommandsRepositoryTest.java
+++ src/Tests/CommandsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +18,43 @@ class CommandsRepositoryTest {
     void pwd() throws IOException {
         _commandRepository = new CommandsRepository();
         assertInstanceOf(String.class, _commandRepository.pwd(new File(".")));
+    }
+
+    @Test
+    @DisplayName("cd subfolder one time")
+    void cd() throws Exception{
+        _commandRepository = new CommandsRepository();
+        ArrayList<String> array = new ArrayList<String>();
+        array.add("src/Tests");
+        File dir = new File("D:\\Programming\\CLIProject");
+        System.out.println(_commandRepository.cd(dir,array));
+        assertEquals("D:\\Programming\\CLIProject/src/Tests", _commandRepository.cd(dir,array));
+    }
+
+    @Test
+    @DisplayName("cd spaced folder")
+    void cdSpaced() throws Exception{
+        _commandRepository = new CommandsRepository();
+        ArrayList<String> array = new ArrayList<String>();
+        array.add("Space Test");
+        File dir = new File("D:\\Programming\\CLIProject");
+        System.out.println(_commandRepository.cd(dir,array));
+        assertEquals("D:\\Programming\\CLIProject/Space Test", _commandRepository.cd(dir,array));
+    }
+
+    @Test
+    @DisplayName("cd not found")
+    void cdException() throws Exception{
+        _commandRepository = new CommandsRepository();
+        ArrayList<String> array = new ArrayList<String>();
+        array.add("abcd");
+        File dir = new File("D:\\Programming\\CLIProject");
+        boolean thrown = false;
+        try {
+            _commandRepository.cd(dir,array);
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 }
