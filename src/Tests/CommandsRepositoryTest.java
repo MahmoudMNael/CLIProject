3add61e65860e24b12d9313fe955098ef063b03e
Mahmoud Mohammed Nael <mahmoudmnael@gmail.com>
2024-10-31 09:17:04 +0300

    1. Implemented mkdir command 2. Added the unittests for mkdir feature

--- src/Tests/CommandsRepositoryTest.java
+++ src/Tests/CommandsRepositoryTest.java
@@ -3,19 +3,55 @@ package Tests;
 import Repositories.CommandsRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandsRepositoryTest {
     private CommandsRepository _commandRepository;
 
     @Test
-    @DisplayName("it succeeds when calling the function")
+    @DisplayName("pwd succeeds when calling the function")
     void pwdSuccess() throws IOException {
         _commandRepository = new CommandsRepository();
         assertInstanceOf(String.class, _commandRepository.pwd(new File(".")));
+    }
+
+    // Test will not succeed twice in a row.
+    // You should delete the created directory or run the rmdir test before running it again
+    // Or run the whole test class bundle
+    @Test
+    @DisplayName("mkdir succeeds when given valid inputs")
+    void mkdirSuccess() throws Exception {
+        _commandRepository = new CommandsRepository();
+        assertDoesNotThrow(() -> _commandRepository.mkdir(new ArrayList<>(Arrays.asList("./testdirectory"))));
+    }
+
+    @Test
+    @DisplayName("mkdir fails when not given any input")
+    void mkdirFailureEmptyInput() throws Exception {
+        _commandRepository = new CommandsRepository();
+        Exception exception = assertThrows(Exception.class, () -> _commandRepository.mkdir(new ArrayList<>()));
+        assertEquals("mkdir: Missing operand!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("mkdir fails when given an already existing directory")
+    void mkdirFailureExistingDirectory() throws Exception {
+        _commandRepository = new CommandsRepository();
+        Exception exception = assertThrows(Exception.class, () -> _commandRepository.mkdir(new ArrayList<>(Arrays.asList("src"))));
+        assertEquals("mkdir: Directory already exists!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("mkdir fails when given a non valid path")
+    void mkdirFailureNoDirectory() throws Exception {
+        _commandRepository = new CommandsRepository();
+        Exception exception = assertThrows(Exception.class, () -> _commandRepository.mkdir(new ArrayList<>(Arrays.asList("blahblah/testdir"))));
+        assertEquals("mkdir: No such file or directory!", exception.getMessage());
     }
 }
